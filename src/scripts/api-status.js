c36a2b8e95fbf6ef1fe987ac0f8e738ff924f3d0
Andy Royle <ajroyle@gmail.com>
2013-10-26 12:43:19 +0100

    add ability to set new aliases

--- src/scripts/api-status.js
+++ src/scripts/api-status.js
@@ -10,6 +10,8 @@
 // Commands:
 //   hubot status <application> <monitor> [<server>] - Returns the current state of the given application
 //   hubot status show aliases - List all aliases
+//   hubot set alias <app> <server> <url> - Stores a new alias for the specified application
+var Url = require('url');
 
 var aliases = {
   review: {
@@ -36,6 +38,10 @@ module.exports = function(robot){
 
   robot.respond(/status show aliases$/i, function(msg){
     showAliases(msg);
+  });
+
+  robot.respond(/status set alias (\S+) (\S+) (\S+)/i, function(msg){
+    setAlias(msg);
   });
 };
 
@@ -112,4 +118,22 @@ getServersForAlias = function(app){
     res.push(s);
   }
   return res;
+},
+
+setAlias = function(msg){
+    var bits = {
+        app: msg.match[1],
+        server: msg.match[2],
+        url: msg.match[3]
+    };
+
+    var url = Url.parse(bits.url);
+
+    if(!url.protocol || !url.host){
+        msg.send("Check yo damn urls dawg");
+        return;
+    }
+
+    aliases[bits.app][bits.server] = bits.url;
+    msg.send('Ok, alias for ' + bits.app + ' ' + bits.server + ' was set');
 };
