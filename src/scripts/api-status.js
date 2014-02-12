c697dcbcfa1fc25e5eb358c55dc82a7dc8018200
Paul Stack <public@paulstack.co.uk>
2014-02-12 19:59:06 +0000

    Update api-status.js
    adding the matching whitespace character on the regex. Stupid regex!

--- src/scripts/api-status.js
+++ src/scripts/api-status.js
@@ -49,7 +49,7 @@ module.exports = function(robot){
     lbstatus(msg);
   });
 
-  robot.respond(/buildversion (\S+) (\S+)?$/i, function(msg){
+  robot.respond(/buildversion (\S+)(\s\S+)?$/i, function(msg){
     buildversion(msg);
   });
 };
